c41c2b669484d0fa08ebe564b96bfa209311cb8e
Ankit Goyal <ankit3goyal@gmail.com>
2016-03-02 20:13:50 -0800

    Initial commit

---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+
+get '/:mp_name' do
+  @mp_name = params[:mp_name]
+
+  @name = ENV['USER']
+
+  @test_data = [
+    {class: "LoginViewTests", method: "testHappyPath", pass: 12, fail: 54, days: 40},
+    {class: "ProfileViewTests", method: "testHappyPath", pass: 132, fail: 54, days: 40},
+    {class: "SettingsViewTests", method: "testHappyPath", pass: 42, fail: 14, days: 40}
+  ]
+  erb :main
+end
+
+get '/:mp_name/:test_class/:test_method' do
+  @mp_name = "cool"
+  @test_data = []
+  erb :class_test_method
+end
+
+get '/:mp_name/test_method' do
+  
+end
+
